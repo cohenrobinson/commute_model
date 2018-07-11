8acc335c7cc250dc2a93bff2e6494085d2ca8faf
Cohen <cohenrobinson@icloud.com>
2018-07-11 21:29:23 +1000

    Added model & agents to model.py. Behaviour of the agents and model also

---
+++ commute_model/model.py
@@ -0,0 +1,21 @@
+import random
+
+from mesa import Model, Agent
+from mesa.space import SingleGrid
+from mesa.time import RandomActivation
+from mesa.datacollection import DataCollector
+
+class CommuteModel(Model):
+    """This agent-based model seeks to demonstrate the effect of spatial
+    inequality on the way agents commute."""
+
+    def __init__(self, ):
+
+class CommuteAgent(Agent):
+    """An agent that follows behaviour shown in the README.md."""
+    def __init(self, unique_id, pos):
+
+class CityAgent(Agent):
+    """An agent representing the city centre."""
+    def __init__(self, pos):
+        self.
